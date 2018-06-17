e71bf5033d61b5f902f870ce9fc5da8e82c89ec2
huruifu <loveshiki1996@gmail.com>
2018-06-16 20:22:48 -0700

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', 'root', 'NBA');
+$connection = mysqli_connect('localhost', 'root', 'root', 'newNBA');
 if (!$connection) {
     die("CONNECTION FAILED");
 }
